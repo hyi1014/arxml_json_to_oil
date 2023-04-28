05c61f55cffb949b6e89f4ca70c03080728a42d5
hyi1014 <hyi28915088@gmail.com>
2023-04-28 11:37:23 +0800

    check spec 0

--- arxml_json_to_oil.py
+++ arxml_json_to_oil.py
@@ -7,19 +7,6 @@ input_f = open("application_Os_ecuc.json")
 #output_file
 output_f = open("output.oil", "w")
 
-class FileType(enum.Enum):
-    ecu = 1
-    swc = 2
-#function to get the file type
-def get_file_type(data):
-    if(data["SHORT-NAME"].find("Ecu")):
-        return FileType.ecu
-    elif(data["SHORT-NAME"].find("SAR")):
-        return FileType.swc
-    else:
-        print("Error: file type not found")
-        exit(1)
-
 #input: the dict of CONTAINERS
 #output: the oil strucutre of the CONTAINERS
 def print_prefix_tabs(string, num_tabs):
@@ -57,34 +44,14 @@ def recursive_parse_val(elements, num_tabs):
 #json objects to python dict
 data = json.load(input_f)
 data = data["AUTOSAR"]["AR-PACKAGES"]["AR-PACKAGE"]
-#get the file type
-f_type = get_file_type(data)
-if(f_type == FileType.ecu):
-    data = data["ELEMENTS"]["ECUC-MODULE-CONFIGURATION-VALUES"]["CONTAINERS"]["ECUC-CONTAINER-VALUE"]
-elif(f_type == FileType.swc):
-    data = data["AR-PACKAGE"]
-
-
+data = data["ELEMENTS"]["ECUC-MODULE-CONFIGURATION-VALUES"]
+data = data["CONTAINERS"]["ECUC-CONTAINER-VALUE"]
 
 import sys
 with output_f as sys.stdout:
     for elements in data:
-        # #list the keys
-        # elements_keys = list(elements.keys())
-        # #type of the element
-        # print(str(elements[elements_keys[1]]).split("/")[-1] ,end=" ")
-        # #name of the element
-        # print(elements[elements_keys[0]])
-        # print("{")
-        # if(len(elements_keys)>2):
         recursive_parse_val(elements, 0)
-            # if(elements_keys[2] == "PARAMETER-VALUES"):
-            #     for args_class_key in elements[elements_keys[2]]:
-            #         for args in elements[elements_keys[2]][args_class_key]:
-            #             print(args)
-                # for args_keys, args_val in args.items():
-                #     print("\t" + args_keys + " = " + args_val)
-        # print("}")
+
 #close the files
 input_f.close()
 output_f.close()
